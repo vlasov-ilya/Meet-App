5a470435e9d7bbe70f5fb48e854a70aa5db0077c
Ilya Vlasov <ilyavlasov93@gmail.com>
2020-12-10 22:12:32 -0500

    Test set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CitySearch />
+        <EventList />
+        <NumberOfEvents />
+      </div>
+    );
+  }
 }
+
 
 export default App;
